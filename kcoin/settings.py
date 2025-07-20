cee9d34ea2608b9d0c194259747f4c81427e5038
supraja-shetty <suprajashetty84@gmail.com>
2025-07-20 13:16:26 +0530

    Fix: Added Render domain to ALLOWED_HOSTS

--- kcoin/settings.py
+++ kcoin/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = 'django-insecure-xyz1234567890abcdef!@#$%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
